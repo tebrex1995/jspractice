3c9dad6bfd91c24fb317f21d7613c4fce35b46d6
Aleksa <tvrdisica@gmail.com>
2023-02-05 21:13:27 +0100

    started new part

---
+++ part2/assignments.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// function describeCountry(country, population, capitalCity) {
+//   const statement = `${country} has ${population} milion people and its capital city is ${capitalCity}`;
+//   return statement;
+// }
+
+// const serbia = describeCountry("Serbia", 6.8, "Belgrade");
+// const vatikan = describeCountry("Vatikan", 0.02, "Vatikan");
+// const belgium = describeCountry("Belgium", 11.59, "Brussels");
+
+// console.log(serbia);
+// console.log(vatikan);
+// console.log(belgium);
+
+//functional declaration
+
+// function percentageOfWorld1(country, population) {
+//   console.log(
+//     `${country} has ${population}, so its about ${
+//       (population / 7900) * 100
+//     } of the world population. `
+//   );
+// }
+
+// percentageOfWorld1("Serbia", 6.8);
+// percentageOfWorld1("Belgium", 11.59);
+// percentageOfWorld1("utopia", 1000);
+
+// const percentage = function (country, population) {
+//   return `${country} has ${population}, so its about ${
+//     (population / 7900) * 100
+//   } of the world population. `;
+// };
+
+// console.log(percentage("Serbia", 6.8));
---
+++ part2/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// function logger() {
+//   console.log("My name is Aleksa");
+// }
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+// function calcAge1(birthYear) {
+//   const age = 2023 - birthYear;
+//   return age;
+// }
+
+// const age1 = calcAge1(1995);
+// console.log(age1);
+
+const calcAge2 = function (birthYear) {
+  const age = 2023 - birthYear;
+  return age;
+};
+
+const age2 = calcAge2(1991);
+
+console.log(age2);
